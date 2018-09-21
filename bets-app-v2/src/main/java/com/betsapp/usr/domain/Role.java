76e44b82322d0105bb9842bbddd7e85395059c44
Alejandro Niño <alejandro6230@gmail.com>
2018-09-21 23:33:46 +0200

    Add role constraint

--- bets-app-v2/src/main/java/com/betsapp/usr/domain/Role.java
+++ bets-app-v2/src/main/java/com/betsapp/usr/domain/Role.java
@@ -7,9 +7,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
-@Table(name = "USR_ROLES")
+@Table(name = "USR_ROLES", uniqueConstraints = { @UniqueConstraint(columnNames = { "user_id", "name" })})
 public class Role implements Serializable {
 
 	private static final long serialVersionUID = 1776131396471519611L;
