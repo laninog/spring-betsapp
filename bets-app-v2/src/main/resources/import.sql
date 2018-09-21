501b8200e58d89ad214917eaccb954a945ed3b61
Alejandro Niño <alejandro6230@gmail.com>
2018-09-21 23:34:19 +0200

    Add users and roles data

--- bets-app-v2/src/main/resources/import.sql
+++ bets-app-v2/src/main/resources/import.sql
@@ -2,3 +2,13 @@ insert into MGR_MATCHES(id, local, rel_local, visitor, rel_visitor, rel_draw, op
 insert into MGR_MATCHES(id, local, rel_local, visitor, rel_visitor, rel_draw, open, close) values(2, 'Corea', 2, 'Polonia', 4, 5, {ts '2018-03-04 11:47:00.00'}, {ts '2018-03-05 21:47:52.00'});
 insert into MGR_MATCHES(id, local, rel_local, visitor, rel_visitor, rel_draw, open, close) values(3, 'Brazil', 6, 'Argentina', 4, 2, {ts '2018-03-04 11:47:00.00'}, {ts '2018-03-05 19:47:52.00'});
 insert into MGR_MATCHES(id, local, rel_local, visitor, rel_visitor, rel_draw, open, close) values(4, 'Camerun', 2, 'USA', 6, 4, {ts '2018-03-04 11:47:00.00'}, {ts '2018-03-05 17:47:52.00'});
+
+/* users */
+insert into USR_USERS (username, password, enabled) values('user', '$2a$04$.50lAnVItLwL0hIVUv2s/e2Ty1rGIXBKZ12AWozjpekF8fABTumcK',1 );
+insert into USR_USERS (username, password, enabled) values('admin', '$2a$04$.50lAnVItLwL0hIVUv2s/e2Ty1rGIXBKZ12AWozjpekF8fABTumcK', 1);
+
+/* roles */
+insert into USR_ROLES (user_id, name) values(1, 'ROLE_USER');
+insert into USR_ROLES (user_id, name) values(2, 'ROLE_USER');
+insert into USR_ROLES (user_id, name) values(2, 'ROLE_ADMIN');
+insert into USR_ROLES (user_id, name) values(2, 'ROLE_REPORT');
