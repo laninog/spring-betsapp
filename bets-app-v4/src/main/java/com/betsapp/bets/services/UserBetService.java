8d43fbddf0b8ee7f310ebf2dd0ed7ff80019546d
Alejandro Niño <alejandro6230@gmail.com>
2018-10-05 16:56:51 +0200

    Add create method

--- bets-app-v4/src/main/java/com/betsapp/bets/services/UserBetService.java
+++ bets-app-v4/src/main/java/com/betsapp/bets/services/UserBetService.java
@@ -1,11 +1,19 @@
 package com.betsapp.bets.services;
 
+import com.betsapp.bets.domain.UserBet;
 import com.betsapp.bets.mappers.UserBetMapper;
 import com.betsapp.bets.repositories.UserBetRepository;
+import com.betsapp.mgr.domain.Match;
+import com.betsapp.mgr.repositories.MatchRepository;
+import com.betsapp.usr.domain.User;
+import com.betsapp.usr.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserBetService {
@@ -16,8 +24,29 @@ public class UserBetService {
     @Autowired
     private UserBetRepository userBetRespository;
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private MatchRepository matchRepository;
+
     public List<UserBetDTO> findAll() {
         return mapper.toDto(userBetRespository.findAll());
+    }
+
+    @Transactional
+    public UserBetDTO create(UserBetDTO userBet) {
+
+        Optional<User> user = userRepository.findById(userBet.getUser());
+        user.orElseThrow(() -> new Error("User not found"));
+
+        Optional<Match> match = matchRepository.findByIdOptional(userBet.getMatch());
+        match.orElseThrow(() -> new Error("Match not found"));
+
+        UserBet newUserBet = mapper.toEntity(userBet);
+        newUserBet.setCreated(LocalDateTime.now());
+
+        return mapper.toDto(userBetRespository.save(newUserBet));
     }
 
 }
