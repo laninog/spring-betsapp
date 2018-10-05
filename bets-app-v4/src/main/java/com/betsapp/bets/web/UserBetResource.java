abe39c1e3b8d25f67aadef572df2f96fb8858d61
Alejandro Niño <alejandro6230@gmail.com>
2018-10-05 16:57:30 +0200

    Add create method

--- bets-app-v4/src/main/java/com/betsapp/bets/web/UserBetResource.java
+++ bets-app-v4/src/main/java/com/betsapp/bets/web/UserBetResource.java
@@ -1,11 +1,10 @@
 package com.betsapp.bets.web;
 
+import com.betsapp.BetsAppProperties;
 import com.betsapp.bets.services.UserBetDTO;
 import com.betsapp.bets.services.UserBetService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,8 +15,12 @@ public class UserBetResource {
     @Autowired
     private UserBetService userBetService;
 
-    @RequestMapping(value = "/user-bets", method = RequestMethod.GET,
-        produces = "application/json")
+    @PostMapping("/user-bets")
+    public UserBetDTO create(@RequestBody UserBetDTO userBet) {
+        return userBetService.create(userBet);
+    }
+
+    @GetMapping("/user-bets")
     public List<UserBetDTO> findAll() {
         return userBetService.findAll();
     }
