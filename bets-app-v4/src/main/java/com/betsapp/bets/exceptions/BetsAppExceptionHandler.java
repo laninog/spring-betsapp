a602bcbf79a855d778a4253a8f723e87e12da3bd
Alejandro Niño <alejandro6230@gmail.com>
2018-10-07 16:13:36 +0200

    Create global exception handler

---
+++ bets-app-v4/src/main/java/com/betsapp/bets/exceptions/BetsAppExceptionHandler.java
@@ -0,0 +1,65 @@
+package com.betsapp.bets.exceptions;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Locale;
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class BetsAppExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Autowired
+    private MessageSource messages;
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request) {
+
+        Locale locale = request.getLocale();
+
+        BindingResult result = ex.getBindingResult();
+
+        List<String> collect = result.getAllErrors().stream()
+                .map(e -> messages.getMessage(e, locale))
+                .collect(Collectors.toList());
+
+        BetsAppExceptionResponse exceptionResponse =
+                new BetsAppExceptionResponse(LocalDateTime.now(), "Bad request",
+                        collect.toString());
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Object> handleNotFoundException(Exception ex, WebRequest request) {
+
+        BetsAppExceptionResponse exceptionResponse =
+                new BetsAppExceptionResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
+
+        BetsAppExceptionResponse exceptionResponse =
+                new BetsAppExceptionResponse(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
+
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+}
