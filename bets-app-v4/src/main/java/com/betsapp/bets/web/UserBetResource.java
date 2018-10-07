af726f0f468522f719e51db8c9558943a2baeaa5
Alejandro Niño <alejandro6230@gmail.com>
2018-10-07 16:52:58 +0200

    Add HATEOAS to resource

--- bets-app-v4/src/main/java/com/betsapp/bets/web/UserBetResource.java
+++ bets-app-v4/src/main/java/com/betsapp/bets/web/UserBetResource.java
@@ -4,8 +4,13 @@ import com.betsapp.BetsAppProperties;
 import com.betsapp.bets.services.UserBetDTO;
 import com.betsapp.bets.services.UserBetService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Resource;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -16,13 +21,31 @@ public class UserBetResource {
     private UserBetService userBetService;
 
     @PostMapping("/user-bets")
-    public UserBetDTO create(@RequestBody UserBetDTO userBet) {
-        return userBetService.create(userBet);
+    public ResponseEntity<UserBetDTO> create(@Valid @RequestBody UserBetDTO userBet) {
+        UserBetDTO userBetDTO = userBetService.create(userBet);
+        ResponseEntity<UserBetDTO> res = new ResponseEntity<>(userBetDTO, HttpStatus.CREATED);
+        return res;
     }
 
     @GetMapping("/user-bets")
     public List<UserBetDTO> findAll() {
         return userBetService.findAll();
+    }
+
+    @GetMapping("/user-bets/{id}")
+    public Resource<UserBetDTO> findById(@PathVariable("id") Long id) {
+        UserBetDTO userBet = userBetService.findById(id);
+
+        Resource<UserBetDTO> resource = new Resource<>(userBet);
+
+        // HATEOAS
+        ControllerLinkBuilder link =
+                ControllerLinkBuilder.linkTo(
+                        ControllerLinkBuilder.methodOn(this.getClass()).findAll());
+
+        resource.add(link.withRel("findAll"));
+
+        return resource;
     }
 
 }
