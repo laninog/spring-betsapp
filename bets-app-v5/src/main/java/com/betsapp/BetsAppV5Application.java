7268e28389444a47b7cad220e52f440b556e961e
Alejandro Niño <alejandro6230@gmail.com>
2018-10-22 18:00:05 +0200

    Fix main application file name

--- bets-app-v5/src/main/java/com/betsapp/BetsAppV5Application.java
+++ bets-app-v5/src/main/java/com/betsapp/BetsAppV5Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BetsAppV4Application {
+public class BetsAppV5Application {
 	
 	public static void main(String[] args) {
-		SpringApplication.run(BetsAppV4Application.class, args);
+		SpringApplication.run(BetsAppV5Application.class, args);
 	}
 
 }
