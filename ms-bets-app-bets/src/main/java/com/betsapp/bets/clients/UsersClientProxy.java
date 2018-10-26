e9848ae66fe59148dc99d8cf971b104f1d7753f0
Alejandro Niño <alejandro6230@gmail.com>
2018-10-26 20:31:10 +0200

    Eureka, API Gateway

--- ms-bets-app-bets/src/main/java/com/betsapp/bets/clients/UsersClientProxy.java
+++ ms-bets-app-bets/src/main/java/com/betsapp/bets/clients/UsersClientProxy.java
@@ -1,14 +1,13 @@
 package com.betsapp.bets.clients;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="usr-ms")
 // @FeignClient(name="usr-ms")
-//@FeignClient(name="api-gateway")
-@RibbonClient(name="usr-ms")
+// @FeignClient(name="usr-ms")
+@FeignClient(name="api-gateway")
+// @RibbonClient(name="usr-ms")
 public interface UsersClientProxy {
 	
 	// @GetMapping("/users/{id}")
--- naming-server/src/main/java/com/betsapp/NamingServerApplication.java
+++ naming-server/src/main/java/com/betsapp/NamingServerApplication.java
@@ -2,13 +2,13 @@ package com.betsapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-public class MsBetsAppUsrApplication {
+@EnableEurekaServer
+public class NamingServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsBetsAppUsrApplication.class, args);
+		SpringApplication.run(NamingServerApplication.class, args);
 	}
 }
