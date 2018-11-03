00441a0f4bbd22d94383d05dde75a9f3b45ecd26
Alejandro Niño <alejandro6230@gmail.com>
2018-11-03 15:04:00 +0100

    Create matches client proxy

---
+++ ms-bets-app-bets/src/main/java/com/betsapp/bets/clients/MatchesClientProxy.java
@@ -0,0 +1,16 @@
+package com.betsapp.bets.clients;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+// @FeignClient(name="mgr-ms", url="localhost:8100")
+// @FeignClient(name="mgr-ms")
+@FeignClient(name="api-gateway")
+// @RibbonClient(name="mgr-ms")
+public interface MatchesClientProxy {
+	
+	@GetMapping("/matches/{id}")
+	public Match findById(@PathVariable("id") Long id);
+
+}
